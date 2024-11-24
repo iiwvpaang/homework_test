b1dc583c7b6589b1b79c0cf3473830817487ff19
Maneechote <maneechote@gmail.com>
2024-11-25 01:31:30 +0700

    feat(shopping): show products on shopping page

--- lib/screens/latest_product.dart
+++ lib/screens/latest_product.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import '../app_config.dart';
+import '../models/shopping_product.dart';
+import '../services/api_service.dart';
+import '../widgets/product_view.dart';
 import '../widgets/title_bar.dart';
 
 class LatestProduct extends StatefulWidget {
@@ -10,11 +15,108 @@ class LatestProduct extends StatefulWidget {
 }
 
 class _LatestProductState extends State<LatestProduct> {
+  final _scrollController = ScrollController(
+    initialScrollOffset: 1,
+    keepScrollOffset: true,
+  );
+  List<ShoppingProduct> latestProducts = <ShoppingProduct>[];
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(_loadMore);
+
+    setState(() {
+      _getLatestProducts();
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _getLatestProducts() async {
+    latestProducts = (await ApiService().getProducts())!;
+
+    /*recommendedProducts
+        .add(ShoppingProduct(id: 1, name: 'monitor', price: 12.5));
+    recommendedProducts.add(ShoppingProduct(id: 2, name: 'mouse', price: 16.5));
+    recommendedProducts
+        .add(ShoppingProduct(id: 3, name: 'keyboard', price: 52.5));
+    recommendedProducts
+        .add(ShoppingProduct(id: 4, name: 'mobile phone', price: 1200.5));*/
+  }
+
+  void _loadMore() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      //
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Column(
       children: [
-        TitleBar(titleName: 'Latest Products'),
+        SizedBox(
+          height:
+              (screenHeight * (1 - AppConfig.recommendedProductsHeightRatio)) -
+                  AppConfig.bottomBarHeight -
+                  70 -
+                  (loading == true ? AppConfig.loadingBarHeight : 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TitleBar(titleName: 'Latest Products'),
+              Expanded(
+                child: latestProducts == null
+                    ? Container(
+                        child: Text('null'),
+                      )
+                    : ListView.builder(
+                        controller: _scrollController,
+                        itemCount: latestProducts.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index == latestProducts.length) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else {
+                            return ProductView(product: latestProducts[index]);
+                          }
+                        },
+                      ),
+              ),
+            ],
+          ),
+        ),
+        (loading == true
+            ? SizedBox(
+                height: AppConfig.loadingBarHeight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 33,
+                      height: 33,
+                      child: CircularProgressIndicator(),
+                    ),
+                    Text(
+                      '   Loading..',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : SizedBox.shrink()),
       ],
     );
   }
--- lib/screens/recommend_product.dart
+++ lib/screens/recommend_product.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:homework_test/app_config.dart';
 
+import '../models/shopping_product.dart';
+import '../services/api_service.dart';
+import '../widgets/product_view.dart';
 import '../widgets/title_bar.dart';
 
 class RecommendProduct extends StatefulWidget {
@@ -11,12 +15,63 @@ class RecommendProduct extends StatefulWidget {
 }
 
 class _RecommendProductState extends State<RecommendProduct> {
+  List<ShoppingProduct> recommendedProducts = <ShoppingProduct>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _getRecommendedProducts();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _getRecommendedProducts() async {
+    recommendedProducts = (await ApiService().getRecommendedProducts())!;
+
+    /*recommendedProducts
+        .add(ShoppingProduct(id: 1, name: 'monitor', price: 12.5));
+    recommendedProducts.add(ShoppingProduct(id: 2, name: 'mouse', price: 16.5));
+    recommendedProducts
+        .add(ShoppingProduct(id: 3, name: 'keyboard', price: 52.5));
+    recommendedProducts
+        .add(ShoppingProduct(id: 4, name: 'mobile phone', price: 1200.5));*/
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TitleBar(titleName: 'Recommend Product'),
-      ],
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return SizedBox(
+      height: screenHeight * AppConfig.recommendedProductsHeightRatio,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TitleBar(titleName: 'Recommend Product'),
+          Expanded(
+            child: recommendedProducts == null
+                ? Container(
+                    child: Text('null'),
+                  )
+                : ListView.builder(
+                    itemCount: recommendedProducts.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == recommendedProducts.length) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else {
+                        return ProductView(product: recommendedProducts[index]);
+                      }
+                    },
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
