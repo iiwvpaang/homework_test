6f5706e5e1efa4a16db6a00d662a554c85336a7a
Maneechote <maneechote@gmail.com>
2024-11-25 01:28:47 +0700

    feat(api): define api url and endpoints as constant

---
+++ lib/api_constant.dart
@@ -0,0 +1,7 @@
+class ApiConstant {
+  static String baseUrl = 'http://192.168.1.39:8080';
+  static String healthcheckEndpoint = '/healthcheck';
+  static String productsEndpoint = '/products';
+  static String recommendedProductsEndpoint = '/recommended-products';
+  static String checkoutEndpoint = '/orders/checkout';
+}
