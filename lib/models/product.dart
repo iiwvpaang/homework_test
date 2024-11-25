c49015719e911098501e081b4d326961c6a7179a
Maneechote <maneechote@gmail.com>
2024-11-25 12:28:29 +0700

    refactor(product): add amount as a new field for product in cart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,6 +2,9 @@ import 'package:hive/hive.dart';
 
 part 'product.g.dart';
 
+/*------------------------------------------------------------
+ * For product in shopping cart
+ *----------------------------------------------------------*/
 @HiveType(typeId: 1) // typeId should be unique for each model
 class Product {
   @HiveField(0) // unique id for each field
@@ -13,5 +16,12 @@ class Product {
   @HiveField(2)
   late double price;
 
-  Product({required this.id, required this.name, required this.price});
+  @HiveField(3)
+  late int amount;
+
+  Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.amount});
 }
--- lib/models/product.g.dart
+++ lib/models/product.g.dart
@@ -20,19 +20,22 @@ class ProductAdapter extends TypeAdapter<Product> {
       id: fields[0] as int?,
       name: fields[1] as String,
       price: fields[2] as double,
+      amount: fields[3] as int,
     );
   }
 
   @override
   void write(BinaryWriter writer, Product obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(4)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
       ..write(obj.name)
       ..writeByte(2)
-      ..write(obj.price);
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.amount);
   }
 
   @override
